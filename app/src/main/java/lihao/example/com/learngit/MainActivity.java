3ad358dc5ab315c392f4140558bbc05eb0f31456
lihao0608 <18600726809@163.com>
2019-05-07 16:50:55 +0800

    Revert "”测试“"
    This reverts commit aa23a01e8b5026c283370938f76f9dfb5204cecb.

---
+++ app/src/main/java/lihao/example/com/learngit/MainActivity.java
@@ -0,0 +1,13 @@
+package lihao.example.com.learngit;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
